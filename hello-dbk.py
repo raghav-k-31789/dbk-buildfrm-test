47ace35fbd9409f7b4faaf3674859b88d76a7f37
k-raghavendra-31789 <k.raghavendra.31789@gmail.com>
2026-04-13 15:41:36 +0530

    testing more

--- hello-dbk.py
+++ hello-dbk.py
@@ -1,3 +1,9 @@
 # Databricks notebook source
+# MAGIC %pip install pyfiglet
+
 # COMMAND ----------
-print("Hello, World!")
+from pyfiglet import Figlet
+
+user_text = input('Enter text to convert to ASCII art: ')
+figlet = Figlet()
+print(figlet.renderText(user_text))
